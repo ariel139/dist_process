0f176d033e67c0d118100ed10251cb08f5159263
Ariel Cohen <ariel06316@gmail.com>
2023-12-14 11:31:21 +0200

    recv+send update

---
+++ comm.py
@@ -0,0 +1,20 @@
+import socket
+
+RECV_SIZE = 1024
+
+
+def send(soc: socket.socket, data):
+
+    if type(data) == str:
+        data = data.encode()
+    data += b'#'
+    soc.send(data)
+
+
+def recive(soc: socket.socket):
+    data = soc.recv(RECV_SIZE)
+    while data[-1] != b'#':
+        data += soc.recv(RECV_SIZE)
+
+    return data[:-1]
+
