11eea19319b474ec575ed0dff830f971c4d28596
ariel139 <ariel06316@gmail.com>
2023-12-16 19:03:19 +0200

    add deug fetuers to comm.py

--- comm.py
+++ comm.py
@@ -1,7 +1,7 @@
 import socket
 
 RECV_SIZE = 1024
-
+DEBUG = True
 
 def send(soc: socket.socket, data):
 
@@ -9,6 +9,7 @@ def send(soc: socket.socket, data):
         data = data.encode()
     data += b'#'
     soc.send(data)
+    if DEBUG: print(f'SENT: {data}')
 
 
 def recive(soc: socket.socket):
@@ -18,6 +19,6 @@ def recive(soc: socket.socket):
         return b""
     while data[-1] != 35:
         data += soc.recv(RECV_SIZE)
-
+    if DEBUG: print(f"RECIVED: {data}")
     return data[:-1]
 
